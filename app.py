d0a4fa5b3c8e73c45424cd5b4195c9853485ce1a
EL MAHJOUBY marouane <marouaneelmahjouby@gmail.com>
2021-10-17 00:07:59 +0100

    Adding database table with it's columns + LOGIN and SIGNUP code

--- app.py
+++ app.py
@@ -1,23 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from flask.helpers import url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
+from wtforms import StringField, PasswordField
 from wtforms.fields.core import IntegerField
 from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy import SQLAlchemy
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 Bootstrap(app)
 app.config['SECRET_KEY'] = 'marouaneelmahjouby'
-app.config['SQLALCHEMY_DATABE_URI'] = 'C:/Users/Marouane/Desktop/PROJECT1/users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///c:/Users/Marouane/Desktop/PROJECT1/database.db'
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(128))
+    lastname = db.Column(db.String(128))
+    email = db.Column(db.String(50),unique=True)
+    address = db.Column(db.String(50))
+    phone = db.Column(db.Numeric(50))
+    password = db.Column(db.String(100))
 
 
 class LoginForm(FlaskForm):
-    firstname = StringField('FIRST NAME', validators=[InputRequired(), Length(min=4,max=30)])
-    password = PasswordField('PASSWORD', validators=[InputRequired(), Length(min=10,max=30)])
+    email = StringField('EMAIL', validators=[InputRequired()])
+    password = PasswordField('PASSWORD', validators=[InputRequired()])
 
 class RegisterFrom(FlaskForm):
     email = StringField('EMAIL', validators=[InputRequired(), Email(message="Invalid email"), Length(max=40)])
@@ -36,14 +45,27 @@ def home():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return form.firstname.data 
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            if user.password == form.password.data:
+                return redirect(url_for('home'))
+        return "<h1>Invalid Username or Password</h1>"
     return render_template('login.html',form=form)
 
 @app.route('/signup',methods=['GET','POST'])
 def signup():
     form = RegisterFrom()
     if form.validate_on_submit():
-        return form.email.data
+        hashed_password = generate_password_hash(form.password.data)
+        #PUTTING DATA CREATED BY USER INTO THE DATABASE
+        new_user = User(email=form.email.data, firstname=form.firstname.data,
+        lastname=form.lastname.data, address=form.address.data, phone=form.phone.data, 
+        password=form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return '<h1> New user has been created successfully </h1>'
+        
     return render_template('sign_up.html',form=form)
 
 
